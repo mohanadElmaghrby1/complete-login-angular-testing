081829d7b45d6323888eed9a2e8441536de2643c
mohannad <mohanad2021996@gmail.com>
2020-02-09 15:32:46 +0200

    backend initial commit

---
+++ src/main/java/com/mohannad/completelogin/CompleteLoginApplication.java
@@ -0,0 +1,13 @@
+package com.mohannad.completelogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompleteLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompleteLoginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mohannad/completelogin/CompleteLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohannad.completelogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompleteLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
